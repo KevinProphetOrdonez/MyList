ea80f4bca3177d27f63796074a4ba3ecc773bbce
ProphetEclipse <ProphetEclipse@10.221.82.233>
2018-02-15 09:30:24 -0500

    created Array Node class

---
+++ MyList/src/ArrayNode.java
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Title:            MyList
+// Files:            {ArrayNode.java, MyApp.java}
+// Semester:         (Computer Programming II) Spring 2018
+//
+// Author:           Kevin Ordonez
+// Email:            KevinOrdonez0511@FloridaPoly.edu
+// CS Login:         KevinOrdonez0511
+//
+///////////////////////////////////////////////////////////////////////////////
+
+public class ArrayNode {
+	
+}
